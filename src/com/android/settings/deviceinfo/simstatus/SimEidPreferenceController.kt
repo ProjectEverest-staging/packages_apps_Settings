0f491166ad92c374905299d4692cd48b5a7d5404
janak <niraulajanak2019@gmail.com>
2024-04-26 18:38:35 +0000

    Settings: fix(SimEidPreferenceController): Handle null preference safely

--- src/com/android/settings/deviceinfo/simstatus/SimEidPreferenceController.kt
+++ src/com/android/settings/deviceinfo/simstatus/SimEidPreferenceController.kt
@@ -49,7 +49,7 @@ class SimEidPreferenceController(context: Context, preferenceKey: String) :
     BasePreferenceController(context, preferenceKey) {
     private var slotSimStatus: SlotSimStatus? = null
     private var eidStatus: EidStatus? = null
-    private lateinit var preference: CustomDialogPreferenceCompat
+    private var preference: CustomDialogPreferenceCompat? = null
     private var coroutineScope: CoroutineScope? = null
     private lateinit var eid: String
 
@@ -69,7 +69,10 @@ class SimEidPreferenceController(context: Context, preferenceKey: String) :
 
     override fun displayPreference(screen: PreferenceScreen) {
         super.displayPreference(screen)
-        preference = screen.findPreference(preferenceKey)!!
+        preference = screen.findPreference<CustomDialogPreferenceCompat>(preferenceKey)
+        if (preference == null) {
+            Log.e(TAG, "Preference with key $preferenceKey not found in PreferenceScreen")
+        }
     }
 
     override fun onViewCreated(viewLifecycleOwner: LifecycleOwner) {
@@ -85,14 +88,14 @@ class SimEidPreferenceController(context: Context, preferenceKey: String) :
         val isAvailable = withContext(Dispatchers.Default) {
             getIsAvailableAndUpdateEid()
         }
-        preference.isVisible = isAvailable
+        preference?.isVisible = isAvailable
         if (isAvailable) {
             val title = withContext(Dispatchers.Default) {
                 getTitle()
             }
-            preference.title = title
-            preference.dialogTitle = title
-            preference.summary = eid
+            preference?.title = title
+            preference?.dialogTitle = title
+            preference?.summary = eid
             updateDialog()
         }
     }
@@ -120,7 +123,7 @@ class SimEidPreferenceController(context: Context, preferenceKey: String) :
     }
 
     private suspend fun updateDialog() {
-        val dialog = preference.dialog ?: return
+        val dialog = preference?.dialog ?: return
         dialog.window?.setFlags(
             WindowManager.LayoutParams.FLAG_SECURE,
             WindowManager.LayoutParams.FLAG_SECURE
@@ -135,7 +138,7 @@ class SimEidPreferenceController(context: Context, preferenceKey: String) :
 
     override fun handlePreferenceTreeClick(preference: Preference): Boolean {
         if (preference.key != preferenceKey) return false
-        this.preference.setOnShowListener {
+        this.preference?.setOnShowListener {
             coroutineScope?.launch { updateDialog() }
         }
         return true
