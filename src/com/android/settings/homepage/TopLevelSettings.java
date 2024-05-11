Colt-Enigma/platform_packages_apps_Settings

Settings: Randomize BaseCamp's summary
Thanks to our dear maintainers for helping make that list. Especially marshmello61
Also a big thank you to fortune (https://en.wikipedia.org/wiki/Fortune_(Unix)) for some of them.

[@Darkstar085 - Drop derpfest strings]
Change-Id: Ice2e6c36aa900eac7a0097e96d04a77ebdf4a1d8
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -53,11 +53,16 @@ import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.drawer.Tile;
 import com.android.settingslib.search.SearchIndexable;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 @SearchIndexable(forTarget = MOBILE)
 public class TopLevelSettings extends DashboardFragment implements SplitLayoutListener,
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     private static final String TAG = "TopLevelSettings";
+    private static final String KEY_BASECAMP = "top_level_base_camp";
     private static final String SAVED_HIGHLIGHT_MIXIN = "highlight_mixin";
     private static final String PREF_KEY_SUPPORT = "top_level_support";
 
@@ -102,6 +107,13 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         super.onAttach(context);
         HighlightableMenu.fromXml(context, getPreferenceScreenResId());
         use(SupportPreferenceController.class).setActivity(getActivity());
+        updateLabSummary();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateLabSummary();
     }
 
     @Override
@@ -373,6 +385,17 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         }
 
         void doForEach(Preference preference);
+    }
+
+    private void updateLabSummary() {
+        Preference basecamp = findPreference(KEY_BASECAMP);
+        if (basecamp != null) {
+            String[] summaries = getContext().getResources().getStringArray(
+                    R.array.basecamp_summaries);
+            Random rnd = new Random();
+            int summNO = rnd.nextInt(summaries.length);
+            basecamp.setSummary(summaries[summNO]);
+        }
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =

ProjectEverest-staging/packages_apps_Settings

src/com/android/settings/homepage/TopLevelSettings.java
@@ -53,11 +53,16 @@ import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.drawer.Tile;
 import com.android.settingslib.search.SearchIndexable;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 @SearchIndexable(forTarget = MOBILE)
 public class TopLevelSettings extends DashboardFragment implements SplitLayoutListener,
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     private static final String TAG = "TopLevelSettings";
+    private static final String KEY_BASECAMP = "top_level_basecamp";
     private static final String SAVED_HIGHLIGHT_MIXIN = "highlight_mixin";
     private static final String PREF_KEY_SUPPORT = "top_level_support";
 
@@ -102,6 +107,13 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         super.onAttach(context);
         HighlightableMenu.fromXml(context, getPreferenceScreenResId());
         use(SupportPreferenceController.class).setActivity(getActivity());
+        updateLabSummary();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateLabSummary();
     }
 
     @Override
@@ -373,6 +385,17 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         }
 
         void doForEach(Preference preference);
+    }
+
+    private void updateLabSummary() {
+        Preference basecamp = findPreference(KEY_BASECAMP);
+        if (basecamp != null) {
+            String[] summaries = getContext().getResources().getStringArray(
+                    R.array.basecamp_summaries);
+            Random rnd = new Random();
+            int summNO = rnd.nextInt(summaries.length);
+            basecamp.setSummary(summaries[summNO]);
+        }
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =